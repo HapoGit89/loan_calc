bd2ad5f29dcc6cc2ab684a20bba1ca0519e3ca1e
Hannes <hannes.porombka@gmail.com>
2023-01-17 13:23:39 +0100

    finished calculator js functions

--- calculator.js
+++ calculator.js
@@ -27,7 +27,7 @@ const rate = document.getElementById("loan-rate");
 
 years.value = 10;
 amount.value = 300000;
-amount.rate = 1.5;
+rate.value = 1.5;
 
 calculateMonthlyPayment(getCurrentUIValues())
 
@@ -36,13 +36,21 @@ calculateMonthlyPayment(getCurrentUIValues())
 // Get the current values from the UI
 // Update the monthly payment
 function update() {
+  document.getElementById('monthly-payment').innerText = calculateMonthlyPayment(getCurrentUIValues())
 }
 
 // Given an object of values (a value has amount, years and rate ),
 // calculate the monthly payment.  The output should be a string
 // that always has 2 decimal places.
 function calculateMonthlyPayment(values) {
-const monthpay = (values.value * (values.rate/100))/(1-((1+(values.rate/100)))^values.years)
+const P = values.amount;
+const i = values.rate/(100*12)
+const n = values.years*12;
+const monthpay = (P*i)/(1-Math.pow((1+i),-n))
+const mprounded =(Math.round(monthpay*100))/100
+const mpString = `${mprounded}`
+return mpString
+
 
 }
 
@@ -50,3 +58,4 @@ const monthpay = (values.value * (values.rate/100))/(1-((1+(values.rate/100)))^v
 // update the UI to show the value.
 function updateMonthly(monthly) {
 }
+
