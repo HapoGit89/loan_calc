af08f591ddb1ea79d617bf686876a880b9bb9e48
Hannes <hannes.porombka@gmail.com>
2023-01-17 14:37:42 +0100

    used toFixed

--- calculator-test.js
+++ calculator-test.js
@@ -8,12 +8,11 @@ describe ("monthly payment calc test", function(){
   it('should throw error when empty value', function(){
     expect(function(){calculateMonthlyPayment({amount: 600000, years: '', rate: 0.5,})}).toThrowError()
   }
-
-
 )});
 
 
 it("should return a result with 2 decimal places", function() {
+  expect(calculateMonthlyPayment({amount: 100000, years: 20, rate: 0.4,}).charAt((calculateMonthlyPayment({amount: 100000, years: 20, rate: 0.4,}).length)- 3)).toEqual(".")
   // ..
 });
 
